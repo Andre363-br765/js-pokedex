7c9863ec4b83da9e6729c2a34899e0d0540447d6
André <andrefragra@gmail.com>
2025-12-22 16:10:23 -0300

    adicionar

--- src/js/pokemon-api/render.js
+++ src/js/pokemon-api/render.js
@@ -1,18 +1,23 @@
 export function convertPokemonToLi(pokemon) {
-    return `
+  return `
     <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map(t => `<li class="type ${t}">${t}</li>`).join('')}
-            </ol>
-            <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-    </li>`
+        <a href="./src/pages/pokemon-detail.html?id=${pokemon.number}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+                      .map((t) => `<li class="type ${t}">${t}</li>`)
+                      .join("")}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </a>
+    </li>`;
 }
 
 export function renderPokemonList(pokemonListElement, pokemons) {
-    const newHtml = pokemons.map(convertPokemonToLi).join('')
-    pokemonListElement.innerHTML += newHtml
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonListElement.innerHTML += newHtml;
 }
--- src/js/pokemon-detail.js
+++ src/js/pokemon-detail.js
@@ -1,5 +1,3 @@
-import { pokeApi } from './pokemon-api/poke-api'
-
 const params = new URLSearchParams(window.location.search)
 const pokemonId = params.get('id')
 
@@ -9,15 +7,34 @@ function loadPokemonDetail(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then(res => res.json())
         .then(pokemon => {
+            const types = pokemon.types.map(t => t.type.name)
+            const abilities = pokemon.abilities.map(a => a.ability.name)
+
             container.innerHTML = `
-                <h2>${pokemon.name}</h2>
+                <button onclick="history.back()">⬅ Voltar</button>
+
+                <h1 class="${types[0]}">${pokemon.name}</h1>
+
                 <img src="${
                     pokemon.sprites.other.dream_world.front_default ||
                     pokemon.sprites.front_default
                 }" alt="${pokemon.name}">
-                <p><strong>ID:</strong> ${pokemon.id}</p>
-                <p><strong>Altura:</strong> ${pokemon.height}</p>
-                <p><strong>Peso:</strong> ${pokemon.weight}</p>
+
+                <p><strong>ID:</strong> #${pokemon.id}</p>
+                <p><strong>Tipos:</strong> ${types.join(', ')}</p>
+                <p><strong>Altura:</strong> ${pokemon.height / 10} m</p>
+                <p><strong>Peso:</strong> ${pokemon.weight / 10} kg</p>
+                <p><strong>Habilidades:</strong> ${abilities.join(', ')}</p>
+
+                <h2>Status Base</h2>
+                <ul class="stats">
+                    ${pokemon.stats.map(stat => `
+                        <li>
+                            <strong>${stat.stat.name}:</strong>
+                            ${stat.base_stat}
+                        </li>
+                    `).join('')}
+                </ul>
             `
         })
 }
